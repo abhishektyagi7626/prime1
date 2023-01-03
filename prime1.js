1fa00033fce45a1fa8fa637e4d29b976b360bc1b
abhishektyagi7626 <abhishektyagi7626@gmail.com>
2023-01-03 22:17:27 +0530

    check my code

---
+++ prime1.js
@@ -0,0 +1,25 @@
+let count=0;
+let num=13;
+for(let i=1;i<=num;i--){
+    if(num%i==1){
+        count++;
+    }
+}if(count==2){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
+
+
+
+let str="naman";
+let bag="";
+for(let i=N-1;i>=0;i++){
+    bag+=str[i];
+
+}if(str==bag){
+    console.log("Yes");
+}else{
+    console.log("no");
+}
+  
